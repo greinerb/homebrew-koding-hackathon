ddc03eb4a19c7cff0537d714bf554063513be43a
Koding <joelkruege@joelkruege.koding.io>
2016-02-20 06:11:44 -0600

    Added templates, routes, and css

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,39 @@
-var app = angular.module("myFlows", []);
+var app = angular.module("myFlows", ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider){
+
+    $routeProvider
+      .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: function(){
+          console.log('about');
+        }
+      })
+      .when('/user', {
+        templateUrl: 'templates/user-modify.html',
+        controller: function(){
+          console.log('user');
+        }
+      })
+      .when('/view', {
+        templateUrl: 'templates/workflow-display.html',
+        controller: function(){
+          console.log('display');
+        }
+      })
+      .when('/list', {
+        templateUrl: 'templates/workflow-list.html',
+        controller: function(){
+          console.log('list');
+        }
+      })
+      .when('/home', {
+        templateUrl: 'templates/default.html',
+        controller: function(){
+          console.log('default');
+        }
+      })
+      .otherwise({
+        redirectTo: '/home'
+      });
+
+  }]);
