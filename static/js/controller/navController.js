6dff1011b930bf86a5fd6c921268e6d80fe9f027
Joel Krueger <joel.kruege@gmail.com>
2016-02-20 21:26:19 -0600

    added login functionality

--- static/js/controller/navController.js
+++ static/js/controller/navController.js
@@ -1,5 +1,6 @@
-app.controller("NavController", [function($scope){
+app.controller("NavController", ["$scope", "$uibModal", "User", function($scope, $uibModal, User){
   this.tabIndex = 1;
+  $scope.loggedIn = false;
 
   this.isSet = function(tab)
   {
@@ -11,5 +12,36 @@ app.controller("NavController", [function($scope){
     this.tabIndex = tab;
   };
 
+  $scope.open = function(size){
+    console.log("in open");
+      var modal = $uibModal.open({
+      animation: true,
+      templateUrl: 'templates/login-modal.html',
+      size: size,
+      controller: "ModalLoginInstanceCtrl",
+      windowTopClass: "ModalWindow"
+    });
+
+    modal.result.then(function(user){
+      console.log(user);
+      User.validateLogin(user).then(function(response){//success
+        console.log(response);
+        //check response code, set loggedIn = true if successful.
+      }).then(function(response){//error
+        console.log(response);
+        $scope.open();
+      });
+      console.log('Modal dismissed at: ' + new Date());
+    });
+  };
   return this;
+}]);
+app.controller('ModalLoginInstanceCtrl',["$scope", "$uibModalInstance", function($scope, $uibModalInstance){
+  $scope.ok = function(){
+    $uibModalInstance.close($scope.user);
+  };
+
+  $scope.cancel = function(){
+    $uibModalInstance.dismiss('cancel');
+  };
 }]);
